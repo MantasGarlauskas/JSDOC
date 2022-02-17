a62d1f37fed6847f335981d58800a889d1a68e3c
Mantas Garlauskas <mantas.garlauskas@gmail.com>
2022-02-17 21:24:04 +0200

    formula update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const car = require("./js/car.js");
-const chat = require("./js/chat.js");
-const sum = require("./js/sum.js");
+/**
+ * Įvedus automobilio marke, degalu kieki bake ir kiek sunaudoja degalu mieste, automagistralėje ir vidutiniškai, grąžinamas sakinys ir suskaiciuojama kiek kilometru gali nuvaziuoti autoomobilis
+ * @param {string} carName Automobilio modelis
+ * @param {number} fuelInTank Degalu kiekis bake
+ * @param {number} consHighway Degalu sąnaudos automagistraleje
+ * @param {number} consAverage Vidutinės degalu sanaudos 
+ * @param {number} consTown Degalų sąnaudos mieste
+ * @returns Sakinys
+ */
 
+function car(carName, fuelInTank, consHighway, consAverage, consTown) {
+    const highwayKm = Math.floor(fuelInTank * 100 / consHighway)
+    const averageKm = Math.floor(fuelInTank * 100 / consAverage)
+    const townKm = Math.floor(fuelInTank * 100 / consTown)
 
-const pirmas = 7;
-const antras = 5;
+    return `${carName} su ${fuelInTank}l baku, gali nuvaziuoti ${highwayKm}km (magistralej, kai sanaudos yra ${consHighway}l/100km), ${averageKm}km (vidutiniskai, kai sanaudos yra ${consAverage}l/100km) ir ${townKm}km (mieste, kai sanaudos yra ${consTown}l/100km).`
+}
 
-const r = sum(pirmas, antras);
-
-const petrasLabas = chat();
-
-const volvo = car('Volvo XC90', 60, 6, 7, 8)
-
-console.log(volvo);
+module.exports = car;
