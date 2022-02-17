96a240c42d54b335d1ffd0e0931585fdcb69be42
Mantas Garlauskas <mantas.garlauskas@gmail.com>
2022-02-17 21:24:15 +0200

    formula update

--- js/car.js
+++ js/car.js
@@ -9,11 +9,11 @@
  */
 
 function car(carName, fuelInTank, consHighway, consAverage, consTown) {
-    const highwayKm = fuelInTank * 100 / consHighway
-    const averageKm = fuelInTank * 100 / consAverage
-    const townKm = fuelInTank * 100 / consTown
+    const highwayKm = Math.floor(fuelInTank * 100 / consHighway)
+    const averageKm = Math.floor(fuelInTank * 100 / consAverage)
+    const townKm = Math.floor(fuelInTank * 100 / consTown)
 
-    return `${carName} su ${fuelInTank}l baku, gali nuvaziuoti ${highwayKm.toFixed()}km (magistralej, kai sanaudos yra ${consHighway}l/100km), ${averageKm.toFixed()}km (vidutiniskai, kai sanaudos yra ${consAverage}l/100km) ir ${townKm.toFixed()}km (mieste, kai sanaudos yra ${consTown}l/100km).`
+    return `${carName} su ${fuelInTank}l baku, gali nuvaziuoti ${highwayKm}km (magistralej, kai sanaudos yra ${consHighway}l/100km), ${averageKm}km (vidutiniskai, kai sanaudos yra ${consAverage}l/100km) ir ${townKm}km (mieste, kai sanaudos yra ${consTown}l/100km).`
 }
 
 module.exports = car;
